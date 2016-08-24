1ccfb048c50ea5dd939ffd3fffa8ce737c1eac04
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2016-08-24 19:06:02 +1000

    Update banners

--- jimcc_m.f90
+++ jimcc_m.f90
@@ -1,3 +1,23 @@
+! Conformal Cubic Atmospheric Model
+    
+! Copyright 2015 Commonwealth Scientific Industrial Research Organisation (CSIRO)
+    
+! This file is part of the Conformal Cubic Atmospheric Model (CCAM)
+!
+! CCAM is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! CCAM is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with CCAM.  If not, see <http://www.gnu.org/licenses/>.
+
+!------------------------------------------------------------------------------
 
 module jimcc_m
 
@@ -1140,7 +1160,7 @@ subroutine lufm(a, ipiv, d)
       if (ajj == 0.0) then
          jm = j - 1
          print *, "failure in lufact: matrix singular, rank=", jm
-         stop
+         stop -1
       endif
       ajji = 1.0/ajj
       do i = jp, m
@@ -1185,7 +1205,7 @@ subroutine invmm(a)
    if ( size(a, 1) /= size(a, 2) ) then
       print*, " Can't calculate inverse of non-square matrix "
       print*, " Shape is ", shape(a)
-      stop
+      stop -1
    end if
 
    m = size(a, 1)
--- setxyz_m.f90
+++ setxyz_m.f90
@@ -1,3 +1,24 @@
+! Conformal Cubic Atmospheric Model
+    
+! Copyright 2015 Commonwealth Scientific Industrial Research Organisation (CSIRO)
+    
+! This file is part of the Conformal Cubic Atmospheric Model (CCAM)
+!
+! CCAM is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! CCAM is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with CCAM.  If not, see <http://www.gnu.org/licenses/>.
+
+!------------------------------------------------------------------------------
+    
 module setxyz_m
    use jimcc_m
    use jimco_m
@@ -451,7 +472,7 @@ subroutine setxyz ( il, jl, kl, npanels, ifull, iquad, diag, id, jd,        &
    if (npanels == 13) then 
       if ( ntang /= 2 ) then 
          print*, " Error - octagon requires ntang=2 "
-         stop
+         stop -1
       end if
       call jimco ( il, iquad, xx4, yy4, zz4 )
       dsfact = (3.0 + 3.0*sqrt(2.0_rx))*il/(2.0*pi)  ! con-octagon 
